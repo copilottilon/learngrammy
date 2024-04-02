8caeef7b3d16abc6e8926573fb88b8e41ff9529c
copilottilon <164805708+copilottilon@users.noreply.github.com>
2024-04-02 20:59:46 +0500

    Update bot.ts

--- bot.ts
+++ bot.ts
@@ -1,6 +1,12 @@
 import { Bot } from "./deps.deno.ts";
 
-export const bot = new Bot(Deno.env.get("BOT_TOKEN") || "1118961443:AAEYU3pDOXFzSX5IulkYsRKjaLG_zHPCOxQ");
+export const bot = new Bot(
+  "1118961443:AAEYU3pDOXFzSX5IulkYsRKjaLG_zHPCOxQ",
+  client: {
+    // We accept the drawback of webhook replies for typing status.
+    canUseWebhookReply: (method) => method === "sendMessage",
+  }
+);
 
 bot.command("start", (ctx) => ctx.reply("Bot ishlavotti."));
 
