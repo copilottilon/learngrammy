fb15b8d8605a3a7f4b81d055a3e5fbbf44492c62
copilottilon <164805708+copilottilon@users.noreply.github.com>
2024-04-02 20:47:57 +0500

    Update bot.ts

--- bot.ts
+++ bot.ts
@@ -2,6 +2,6 @@ import { Bot } from "./deps.deno.ts";
 
 export const bot = new Bot(Deno.env.get("BOT_TOKEN") || "1118961443:AAEYU3pDOXFzSX5IulkYsRKjaLG_zHPCOxQ");
 
-bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
+bot.command("start", (ctx) => ctx.reply("Bot ishlavotti."));
 
 bot.command("ping", (ctx) => ctx.reply(`Pong! ${new Date()} ${Date.now()}`));
