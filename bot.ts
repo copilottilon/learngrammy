2664f561a9b9fd85c6b9ff143058887c507cf420
copilottilon <164805708+copilottilon@users.noreply.github.com>
2024-04-02 21:02:41 +0500

    Update bot.ts

--- bot.ts
+++ bot.ts
@@ -4,11 +4,15 @@ export const bot = new Bot(
   "1118961443:AAEYU3pDOXFzSX5IulkYsRKjaLG_zHPCOxQ", {
     client: {
       // We accept the drawback of webhook replies for typing status.
-      canUseWebhookReply: (method) => method === "sendMessage",
+      canUseWebhookReply: (method) => method === "sendChatAction",
     }
   }
 );
 
-bot.command("start", (ctx) => ctx.reply("Bot ishlavotti."));
+bot.command("start", async (ctx) => {
+  await ctx.reply("Bot ishlavotti."));
+}
 
-bot.command("ping", (ctx) => ctx.reply(`Pong! ${new Date()} ${Date.now()}`));
+bot.command("ping", async (ctx) => {
+  await ctx.reply(`Pong! ${new Date()} ${Date.now()}`));
+}
