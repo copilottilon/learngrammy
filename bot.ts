32a20a1593dd2036e645d9e18eee0a353e11d360
copilottilon <164805708+copilottilon@users.noreply.github.com>
2024-04-02 20:37:02 +0500

    Update bot.ts

--- bot.ts
+++ bot.ts
@@ -1,6 +1,6 @@
 import { Bot } from "./deps.deno.ts";
 
-export const bot = new Bot(Deno.env.get("BOT_TOKEN") || "");
+export const bot = new Bot(Deno.env.get("BOT_TOKEN") || "1118961443:AAEYU3pDOXFzSX5IulkYsRKjaLG_zHPCOxQ");
 
 bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
 
